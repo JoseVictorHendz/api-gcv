53e4fb40f94bee6d9e6a81c4902190d981fe4bfc
Jose Victor Hendz Martins <Jose>
2018-06-28 10:13:22 -0300

    separando em funcoes

--- webservice.py
+++ webservice.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import base64
+import random
 
 import io
 import os
@@ -13,22 +15,32 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "apikey.json"
 
 
 app = Flask(__name__)
-print("teste")
 
 
+def prepararImagemParaSalvar():
+    imagem = request.json['img']
 
+    imagem = imagem.encode("utf8")
 
-@app.route("/retornoApiGoogleVision")
-def index():
-    retorno = []
+    return imagem
 
-    # Instantiates a client
+def salvarImagem(imgBase64):
+
+    hash = random.getrandbits(128)
+    nomeArquivo = str(hash)
+
+    # In Python 2.7
+    fh = open("./imagens/" + nomeArquivo + ".png", "wb")
+    fh.write(imgBase64.decode('base64'))
+    fh.close()
+    return nomeArquivo
+
+def enviarApiGCV(idArquivo):
     client = vision.ImageAnnotatorClient()
 
-    # The name of the image file to annotate
     file_name = os.path.join(
         os.path.dirname(__file__),
-        './urso.jpg')
+        './imagens/' + idArquivo + '.png')
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
@@ -40,16 +52,28 @@ def index():
     response = client.label_detection(image=image)
     labels = response.label_annotations
 
+    return recuperarRetornoApi(labels)
+
+def recuperarRetornoApi(labels):
+    retorno = []
+
     for label in labels:
         retorno.append(label.description)
         print(label.description)
+    return retorno
 
-    return jsonify(retorno)
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#
+@app.route("/retornoApiGoogleVision", methods=['POST'])
+def index():
+    imagem = prepararImagemParaSalvar()
+
+    idArquivo = salvarImagem(imagem)
+
+    labels = enviarApiGCV(idArquivo)
+
+    return jsonify(labels)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
